6285fabfc281f98274736ef8b2cee225e46d5d5c
Anastasiia Shved <ananashw@gmail.com>
2023-07-06 21:33:29 +0530

    Commit 5 (Dependency injection: create private List<Music> musicList, create bean's musicList)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,12 +1,19 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    private List <Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
-    public void setMusic(Music music) {
-        this.music = music;
-    }
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 
     public String getName() {
         return name;
@@ -27,11 +34,13 @@ public class MusicPlayer {
     public MusicPlayer() {
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
 
     public void playMusic() {
-        System.out.println("Playing " + music.getSong());
+        for (Music music:musicList) {
+            System.out.println("Playing " + music.getSong());
+        }
     }
 }
