157945e920af0349c80ab8e9743a6a0b25a29a2b
Anastasiia Shved <ananashw@gmail.com>
2023-07-10 12:56:03 +0530

    Commit 10 (Annotation Qualifier)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,21 +9,19 @@ import java.util.List;
 
 @Component
 public class MusicPlayer {
+    private Music music1;
+    private Music music2;
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
-
-    // IoC
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier ("classicalMusic") Music music1,
+                       @Qualifier ("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
 
     }
 }
